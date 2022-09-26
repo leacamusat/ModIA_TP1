a6e66fcfa97108dc35f7412c589e2a7c040e83f9
Lea <camusat@insa-toulouse.fr>
2022-09-26 14:04:01 +0200

    "conv_ok"

--- mnist_net.py
+++ mnist_net.py
@@ -6,28 +6,41 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# Utiliser un tensorboard :
+from torch.utils.tensorboard import SummaryWriter
+
 class MNISTNet(nn.Module):
     def __init__(self):
         super(MNISTNet, self).__init__()
-        self.conv1 = nn.Conv2d(...)
-        self.conv2 = nn.Conv2d(...)
-        self.pool = nn.MaxPool2d(...)
-        self.fc1 = nn.Linear(...)
-        self.fc2 = nn.Linear(...)
-        self.fc3 = nn.Linear(...)
+        #il y a autant de filtres de convolution que de canaux de sortie de la convolution précédente (comme des sous images)
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=(3,3)) 
+        
+        #prend en arg le nombre de canaux d'entrée, le nombre de canaux de sortie et la taille du filtre de convolution
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=(5,5))
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2) 
+        #prend en argument la taille du filtre de pooling et le stride c'est de combien de pixels on décale le filtre à chaque étape
+        self.fc1 = nn.Linear(in_features=16 * 4 * 4, out_features=128)
+        self.fc2 = nn.Linear(in_features=128, out_features=64)
+        self.fc3 = nn.Linear(in_features=64, out_features=10)
+        self.flatten=nn.Flatten()
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))       # First convolution followed by
+        x = F.relu(self.conv1(x))       # First convolution followed by, relu is in F (torch.nn.functional.relu)
         x = self.pool(x)                # a relu activation and a max pooling#
-        x = ...
-        ...
+        x = F.relu(self.conv2(x)) 
+        x = self.pool(x)   
+        x = self.flatten(x)
+        x = F.relu(self.fc1(x)) 
+        x = F.relu(self.fc2(x)) 
         x = self.fc3(x)
         return x
 
-    def get_features(self, x):
-        pass
-
-
+    def get_features(self, x): #
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4) #idem que flatten
+        return x
+    
 if __name__=='__main__':
     x = torch.rand(16,1,28,28)
     net = MNISTNet()
--- train_mnist.py
+++ train_mnist.py
@@ -14,67 +14,104 @@ from mnist_net import MNISTNet
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def train(net, optimizer, loader, epochs=10):
+def train(net, optimizer, loader, writer, epochs=10): 
     criterion = nn.CrossEntropyLoss()
-    for epoch in range(epochs):
+    for epoch in range(epochs): #pour chaque epoch 
         running_loss = []
-        t = tqdm(loader)
-        for x, y in t:
-            x, y = x.to(device), y.to(device)
-            outputs = net(x)
-            loss = criterion(outputs, y)
-            running_loss.append(loss.item())
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            t.set_description(f'training loss: {mean(running_loss)}')
+        t = tqdm(loader) #barre d'avancée d'exécution
+        for x, y in t: #on prend batch par batch 
+            x, y = x.to(device), y.to(device) #envoyer sur le gpu ou cpu(device)
+            outputs = net(x) #réseau qu'on a défini précédemment sur l'entrée x 
+            loss = criterion(outputs, y) #fonction perte entre la préditction (output) et la sortie réelle
+            running_loss.append(loss.item()) #ajout de la valeur de la fonction perte 
+            optimizer.zero_grad() #met à 0 les différents gradients (pour tous les batchs) 
+            loss.backward() #backward phase
+            optimizer.step() #Performs a single optimization step (parameter update).
+            t.set_description(f'training loss: {mean(running_loss)}') #affichage 
+        writer.add_scalar("training loss", mean(running_loss), epoch)
 
-def test(model, dataloader):
-    test_corrects = 0
+def test(net, dataloader):
+    test_corrects = 0 #
     total = 0
-    with torch.no_grad():
-        for x, y in dataloader:
+    with torch.no_grad(): #on recalcule pas les gradients pendant le test
+        for x, y in dataloader: # chargement des données
             x = x.to(device)
             y = y.to(device)
-            y_hat = model(x).argmax(1)
-            test_corrects += y_hat.eq(y).sum().item()
-            total += y.size(0)
-    return test_corrects / total
+            y_hat = net(x).argmax(1) #softmax : la sortie du RN est celle qui a la proba la plus élevée
+            test_corrects += y_hat.eq(y).sum().item() #compte le nb de predictions correctes
+            # .item() Returns the value of this tensor as a standard Python number.(convertion en int)
+            total += y.size(0) #nombre d'éléments dans le batch 
+    return test_corrects / total #pourcentage total de bonnes prédictions 
 
 if __name__=='__main__':
 
   parser = argparse.ArgumentParser()
+  #instanciation d'un parseur qui permet la saisie de données par l'utilisateur 
   
+  #ajout d'un nouvel argument en précisant son nom, son type, une valeur par défaut et message d'aide
   parser.add_argument('--exp_name', type=str, default = 'MNIST', help='experiment name')
-  parser.add_argument(...)
-  parser.add_argument(...)
-  parser.add_argument(...)
+  parser.add_argument('--batch_size', type=int, default =32, help='size of batch')
+  parser.add_argument('--lr', type=float, default =1e-2, help='learning rate')
+  parser.add_argument('--epochs', type=int, default =50, help='number of epochs')
 
   args = parser.parse_args()
   exp_name = args.exp_name
-  epochs = ...
-  batch_size = ...
-  lr = ...
+  epochs = args.epochs
+  batch_size = args.batch_size
+  lr = args.lr
 
   # transforms
-  transform = transforms.Compose(
+  transform = transforms.Compose( #transform : activer les operations disponibles sur les images
       [transforms.ToTensor(),
-      transforms.Normalize((0.5,), (0.5,))])
+      transforms.Normalize((0.5,), (0.5,))]) 
+      #transformations en tenseur et normalisation 
+      #a tensor image with mean and standard deviation
 
   # datasets
+  #chargement des images + transformations en tenseur et normalisation des images
   trainset = torchvision.datasets.MNIST('./data', download=True, train=True, transform=transform)
   testset = torchvision.datasets.MNIST('./data', download=True, train=False, transform=transform)
 
   # dataloaders
+  #chargement en batch de taille size batch en mélangeant avec num_workers= 2 cad au plus deux workers peuvent mettre les données dans la ram
   trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
   testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
   
-  net = ...
+  
+  net = MNISTNet() #instanciation du modèle
   # setting net on device(GPU if available, else CPU)
   net = net.to(device)
-  optimizer = optim.SGD(...)
-
-  train(...)
-  test_acc = test(...)
+  optimizer = optim.SGD(net.parameters(), lr=lr) #les paramètres sont les poids et biais et sont implicites
+  
+  writer = SummaryWriter(f'runs/MNIST')
+  train(net, optimizer, trainloader, writer, epochs)
+  test_acc = test(net, testloader)
   print(f'Test accuracy:{test_acc}')
   torch.save(net.state_dict(), "mnist_net.pth")
+  #add embeddings to tensorboard
+  perm = torch.randperm(len(trainset.data)) 
+  images, labels = trainset.data[perm][:256], trainset.targets[perm][:256]
+  images = images.unsqueeze(1).float().to(device)
+  with torch.no_grad():
+    embeddings = net.get_features(images)
+    writer.add_embedding(embeddings,
+                  metadata=labels,
+                  label_img=images, global_step=1)
+
+  # save networks computational graph in tensorboard
+  writer.add_graph(net, images)
+  # save a dataset sample in tensorboard
+  img_grid = torchvision.utils.make_grid(images[:64])
+  writer.add_image('mnist_images', img_grid)
+
+
+  #fn : fonction qui met une interface utilisateur sur le site web
+  #input : text, audio ou image
+  #output : test, image, label
+  
+  
+
+  ##lancer la commande suivant en premier dans 
+  # tensorboard --logdir runs
+
+#python train_mnist.py --epochs=10 --lr=1e-3 --batch_size=64
